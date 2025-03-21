e5b626b7b18e0fe7f9f19485bb58d047a802ea0e
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-21 17:33:42 +0100

    [UPD] Ex2 Start Function

---
+++ esercizio2/esercizio2.js
@@ -0,0 +1,10 @@
+//Esercizio 2 Pari e Dispari
+
+//Chiedere all'utente se sceglie Pari o Dispari
+let userChoice = prompt('Perfavore, scegli Pari o Dispari.').toLowerCase();
+console.log('Bene, hai scelto: ' + userChoice);
+
+//Chiedere all'utente un numero da 1 a 5
+let userNumber = parseInt(prompt('Perfavore, scegli un numero da 1 a 5.'));
+console.log('Bene, hai scelto: ' + userNumber);
+
